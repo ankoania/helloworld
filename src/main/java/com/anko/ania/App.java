385c9a1d112425e5060b1c301fd0af2b62e1d408
ankoania <grabowska.anna.ania@gmail.com>
2018-03-05 20:18:24 +0100

    Inital commit

---
+++ src/main/java/com/anko/ania/App.java
@@ -0,0 +1,17 @@
+package com.anko.ania;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+
+    }
+}
---
+++ src/main/java/com/anko/ania/zwierzeta/Pies.java
@@ -0,0 +1,8 @@
+package com.anko.ania.zwierzeta;
+
+public class Pies {
+
+    int wiek;
+    String imie;
+
+}
